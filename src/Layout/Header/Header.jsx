5ea2f7d7a3063e902a64c97aad93fbcc1b2bc0c1
KillianG0 <killiang06@hotmail.fr>
2024-02-25 00:28:20 +0100

    ajout logo cliquable

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -4,16 +4,21 @@ import { NavLink } from 'react-router-dom'
 
 
 function Header() {
-    return (
-      <div className='navbar'>
-       <img src= {Logo} alt="logo Kasa" />
-       <ul>
-              <li>
-                <NavLink to= "/">Accueil</NavLink></li>
-              <li>
-                <NavLink to= "/About">A propos</NavLink></li>
-          </ul>
-      </div>
-    )
-  }
-export default Header
+  return (
+    <div className='navbar'>
+      <NavLink to="/">
+        <img src={Logo} alt="logo Kasa" />
+      </NavLink>
+      <ul>
+        <li>
+          <NavLink to="/">Accueil</NavLink>
+        </li>
+        <li>
+          <NavLink to="/About">A propos</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
